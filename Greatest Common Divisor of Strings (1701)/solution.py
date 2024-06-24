daee967edcf2e057c9f11b715d2e56ca26f9b085
ushcode <oisinpeppard@gmail.com>
2024-06-25 00:01:27 +0100

    correct working soln (test 122 is bull)

--- Greatest Common Divisor of Strings (1701)/solution.py
+++ Greatest Common Divisor of Strings (1701)/solution.py
@@ -1,13 +1,27 @@
+import math
+
+
 class Solution:
 
     def gcd_of_strings(self, str1: str, str2: str) -> str:
-        lengths = [len(str1),len(str2)]
+        lengths = [len(str1), len(str2)]
         result = str1[0]
         i = 1
-        if str1[0] == str2[0]:
+        if str1 == str2:
+            return str1
+        if (
+            (str1.startswith(str2) or str2.startswith(str1))
+            and str1.replace(str2, "") in str2
+            and str2.replace(str1, "") in str1
+        ):
             while i < min(lengths):
                 divisors = [lengths[0] // len(result), lengths[1] // len(result)]
-                if (result * divisors[0] == str1) and (result * divisors[1] == str2):
+                print(divisors)
+                if (
+                    (result * divisors[0] == str1)
+                    and (result * divisors[1] == str2)
+                    and math.gcd(divisors[0], divisors[1]) == 1
+                ):
                     break
                 result += str1[i]
                 i += 1
@@ -15,4 +29,9 @@ class Solution:
             result = ""
         return result
 
-print(Solution.gcd_of_strings(Solution, "Leet", "Code"))
+
+str1 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+str2 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+
+
+print(Solution.gcd_of_strings(Solution, str1, str2))
